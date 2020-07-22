b317f63caef2a71d88ee7d737f8a7102c1ff15ca
CKjapan <koga5958@hotmail.co.jp>
2020-07-23 02:48:13 +0900

    Update models.go

--- server/models/models.go
+++ server/models/models.go
@@ -1,6 +1,5 @@
 package models
 
-// Todo is
 type Todo struct {
 	ID     string `gorm:"column:id;primary_key"`
 	Text   string `gorm:"column:text"`
@@ -8,18 +7,15 @@ type Todo struct {
 	UserID string `gorm:"column:user_id"`
 }
 
-// User is
 type User struct {
 	ID   string `gorm:"column:id;primary_key"`
 	Name string `gorm:"column:name"`
 }
 
-// TableName is
 func (u *Todo) TableName() string {
 	return "todo"
 }
 
-// TableName is
 func (u *User) TableName() string {
 	return "user"
 }
