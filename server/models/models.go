5d15c817a22e09aba8f77b9b8a92ee4366fca69f
YusukeKoga <koga5958@hotmail.co.jp>
2020-07-23 01:25:29 +0900

    first commit

---
+++ server/models/models.go
@@ -0,0 +1,25 @@
+package models
+
+// Todo is
+type Todo struct {
+	ID     string `gorm:"column:id;primary_key"`
+	Text   string `gorm:"column:text"`
+	Done   bool   `gorm:"column:done"`
+	UserID string `gorm:"column:user_id"`
+}
+
+// User is
+type User struct {
+	ID   string `gorm:"column:id;primary_key"`
+	Name string `gorm:"column:name"`
+}
+
+// TableName is
+func (u *Todo) TableName() string {
+	return "todo"
+}
+
+// TableName is
+func (u *User) TableName() string {
+	return "user"
+}
